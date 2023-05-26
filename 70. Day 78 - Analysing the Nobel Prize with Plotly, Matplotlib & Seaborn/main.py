365df425352ed7beece91731757c5d53d9afedcf
Salman Adnan <95875501+grmculmfuer9@users.noreply.github.com>
2023-05-26 19:46:40 +0500

    Update Day 77

---
+++ 70. Day 78 - Analysing the Nobel Prize with Plotly, Matplotlib & Seaborn/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# import numpy as np
+# import plotly.express as px
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+pd.options.display.float_format = '{:,.2f}'.format
+
+df_data = pd.read_csv('nobel_prize_data.csv')
