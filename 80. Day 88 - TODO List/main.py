a03128cd725809679dafb118854d4eee994ffd23
Salman Adnan <grmculmfuer9@gmail.com>
2023-06-04 14:08:52 +0500

    Incomplete Day 88

---
+++ 80. Day 88 - TODO List/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+# , redirect, url_for, flash, abort
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
