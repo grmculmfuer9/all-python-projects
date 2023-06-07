4838ce0b59e97acc564f8e30f1a029ec31bed987
Salman Adnan <95875501+grmculmfuer9@users.noreply.github.com>
2023-06-07 20:30:12 +0500

    Update Day 90

--- 82. Day 90 - Convert PDF to Audiobook/main.py
+++ 82. Day 90 - Convert PDF to Audiobook/main.py
@@ -1,40 +1,43 @@
+import os
 from tkinter import filedialog
+
 from PyPDF2 import PdfReader
 from gtts import gTTS
-import os
 
-f_types = [('PDF files', '*.pdf')]
+filenames = []
 
-filenames = filedialog.askopenfilenames(filetypes=f_types)
+while len(filenames) == 0:
+    f_types = [('PDF files', '*.pdf')]
 
-if len(filenames) == 0:
+    filenames = filedialog.askopenfilenames(filetypes=f_types)
+
     print("No files selected")
-else:
-    for filename in filenames:
-        # Open the PDF file in read-binary mode
-        # Open the PDF file in read-binary mode
-        with open(filename, 'rb') as file:
 
-            # Create a PdfReader object
-            reader = PdfReader(file)
+for filename in filenames:
+    # Open the PDF file in read-binary mode
+    # Open the PDF file in read-binary mode
+    with open(filename, 'rb') as file:
 
-            # Iterate through each page
-            for page in reader.pages:
-                # Extract the text from the page
-                text = page.extract_text()
-                print(text)
+        # Create a PdfReader object
+        reader = PdfReader(file)
 
-        temp1 = filename.split('/')[-1].split('.')[-2]
-        try:
-            open('/'.join(filename.split("/")[:-1]) + "/tts_pdf/", 'r')
-        except FileNotFoundError:
-            os.mkdir('/'.join(filename.split("/")[:-1]) + "/tts_pdf/")
-        except PermissionError:
-            pass
+        # Iterate through each page
+        for page in reader.pages:
+            # Extract the text from the page
+            text = page.extract_text()
+            print(text)
 
-        save_filename = '/'.join(
-            filename.split("/")[:-1]) + "/tts_pdf/" + temp1 + "tts.mp3"
-        print(save_filename)
+    temp1 = filename.split('/')[-1].split('.')[-2]
+    try:
+        open('/'.join(filename.split("/")[:-1]) + "/tts_pdf/", 'r')
+    except FileNotFoundError:
+        os.mkdir('/'.join(filename.split("/")[:-1]) + "/tts_pdf/")
+    except PermissionError:
+        pass
 
-        tts = gTTS(text=text)
-        tts.save(save_filename)
+    save_filename = '/'.join(
+        filename.split("/")[:-1]) + "/tts_pdf/" + temp1 + "tts.mp3"
+    print(save_filename)
+
+    tts = gTTS(text=text)
+    tts.save(save_filename)
